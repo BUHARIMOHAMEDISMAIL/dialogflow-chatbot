a801522a39415a8ad83a9fa77a28f98a48ac312b
Mohamed Ismail <ismailbuharimohamed@gmail.com>
2018-09-14 10:50:29 +0530

    change the path in sendfile

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/adiademochatbot/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 /**
